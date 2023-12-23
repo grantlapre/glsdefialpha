a644535b7da0a2e3d6a19121fe6ed2919e33a413
Lawrie Grant-Lapre <lawr4life@gmail.com>
2023-12-23 09:49:15 +0800

    Buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './components/Header';
 import {useWeb3Contract} from 'react-moralis';
 import {abi} from './constants/abi';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 
 function App() {
@@ -26,6 +28,23 @@ function App() {
       <Container classname="p-3">
        
           <h1 classname="header">GLSDefi Alpha Project</h1>
+          <ButtonGroup>
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>10</Button>
+            <Button>11</Button>
+            <Button>12</Button>
+            <Button>13</Button>
+            <Button>14</Button>
+            <Button>15</Button>
+          </ButtonGroup>
  
       </Container>
       <header className="App-header">
