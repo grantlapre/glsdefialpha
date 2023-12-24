f13eb799de2b0cefa8ca0dbe26898e46ba37647f
Lawrie Grant-Lapre <lawr4life@gmail.com>
2023-12-24 16:31:54 +0800

    dittoinETH

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     contractAddress:"0xd41DD996Ad1a0da8922A8182A64586307136a37c",
     functionName:"publicSaleMint",
     params:{
-      payableAmount: Moralis.Units.ETH(0.6),
+      payableAmount: Moralis.Units.ETH("0.6"),
       _symAmount : 1,
     }
   };
@@ -74,7 +74,7 @@ function App() {
       </Container>
       <Container>
        
-          <h4>Sale State..</h4>
+          <h4>Sale State....</h4>
           <ButtonGroup>
             <Button onClick={()=> runContractFunction({params: pausedSt})}>Paused</Button>
             <Button onClick={()=> runContractFunction({params: saleSt})}>Public Sale</Button>
