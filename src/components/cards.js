a96c342f07bfcbaf46f0df92cd689dc6673e594f
Lawrie Grant-Lapre <lawr4life@gmail.com>
2023-12-25 16:20:39 +0800

    error2

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,7 +2,6 @@
 import Card from 'react-bootstrap/Card';
 import Amount from './amount';
 import glogo from './glsdefiAlphaimg.png';
-import Supply from './supply';
 
 function Cards() {
   return (
@@ -15,15 +14,7 @@ function Cards() {
         </Card.Text>
         <Amount />
       </Card.Body>
-      <Card.Body>
-        <Card.Title>Items</Card.Title>
-        <Card.Text>
-          Backed by Real World Asset 
-        </Card.Text>
-        <Supply />
-      </Card.Body>
     </Card>
-    
   );
 }
 
