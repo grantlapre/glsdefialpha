1a63c040a87db5f92bafec0e033eb29e15007d26
Lawrie Grant-Lapre <lawr4life@gmail.com>
2023-12-25 13:25:18 +0800

    glogo

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,11 +1,12 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Amount from './amount';
+import glogo from './glsdefiAlphaimg.png';
 
 function Cards() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="glsdefiAlphaimg.png" />
+      <Card.Img variant="top" src={glogo} />
       <Card.Body>
         <Card.Title>Mint Amount</Card.Title>
         <Card.Text>
