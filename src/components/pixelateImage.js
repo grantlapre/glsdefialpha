135fed711b68e06c1a68e46a25f9e2e541ec92e8
Lawrie Grant-Lapre <lawr4life@gmail.com>
2023-12-27 09:15:45 +0800

    image8

--- src/components/pixelateImage.js
+++ src/components/pixelateImage.js
@@ -5,6 +5,13 @@ import img2 from './image2.png';
 import img3 from './image3.png';
 import img4 from './image4.png';
 import img5 from './image5.png';
+import img6 from './image1.png';
+import img7 from './image2.png';
+import img8 from './image3.png';
+import img9 from './image4.png';
+import img10 from './image5.png';
+import img11 from './image4.png';
+import img12 from './image5.png';
 
 const images = [
   img1,
@@ -12,6 +19,13 @@ const images = [
   img3,
   img4,
   img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
   // Add more image URLs as needed
 ];
 
